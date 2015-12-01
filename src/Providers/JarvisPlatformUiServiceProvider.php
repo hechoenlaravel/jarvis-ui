a37059fa7a6ebdd3297b625060d2be1eef8a32a7
Jose Luis Fonseca <jose@ditecnologia.com>
2015-12-01 15:49:44 -0500

    update path

--- src/Providers/JarvisPlatformUiServiceProvider.php
+++ src/Providers/JarvisPlatformUiServiceProvider.php
@@ -16,12 +16,12 @@ class JarvisPlatformUiServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        $this->loadViewsFrom(__DIR__.'/../resources/views', 'jplatformui');
+        $this->loadViewsFrom(__DIR__.'/../../resources/views', 'jplatformui');
         $this->publishes([
-            __DIR__.'/../public' => public_path('vendor/jplatformui'),
+            __DIR__.'/../../public' => public_path('vendor/jplatformui'),
         ], 'jplatformpublic');
         $this->publishes([
-            __DIR__.'/../resources/views' => base_path('resources/views/vendor/jplatformui')
+            __DIR__.'/../../resources/views' => base_path('resources/views/vendor/jplatformui')
         ], 'jplatformviews');
     }
 }
